c83f02e8e001a87a8f502599f736e12a6f03ce10
Dweep Panchal <dweep.programmer@gmail.com>
2020-09-22 20:15:09 +0530

    code cleaning

--- src/components/CountdownArea.jsx
+++ src/components/CountdownArea.jsx
@@ -4,9 +4,10 @@ const CountdownArea = () => {
   const calculateTimeLeft = () => {
     const difference = +new Date(2020, 9, 5) - +new Date();
     let timeLeft = {};
-    // console.log("Year: " + year);
-    console.log("Date:" + Date(2020, 10, 5));
-    console.log("Diference: " + difference);
+
+    // console.log("Date:" + Date(2020, 10, 5));
+    // console.log("Diference: " + difference);
+
     if (difference > 0) {
       timeLeft = {
         days: addPrecedingZero(Math.floor(difference / (1000 * 60 * 60 * 24))),
@@ -19,37 +20,49 @@ const CountdownArea = () => {
 
     return timeLeft;
   };
+
   function addPrecedingZero(time) {
-    console.log(time);
+    // console.log(time);
+
     let timeS = String(time);
+
     if (timeS.length <= 1) {
       timeS = "0" + timeS;
     }
-    console.log(timeS);
+
+    // console.log(timeS);
+
     return timeS;
   }
+
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
   useEffect(() => {
     setTimeout(() => {
       setTimeLeft(calculateTimeLeft());
     }, 60000);
   });
+
   const timerComponents = [];
   Object.keys(timeLeft).forEach((interval, index) => {
     if (!timeLeft[interval]) {
       return;
     }
-    console.log(index);
+
+    // console.log(index);
+
     if (index === 2) {
       timerComponents.push(<span>{timeLeft[interval]}</span>);
       return;
     }
+
     timerComponents.push(
       <span>
         {timeLeft[interval]} {": "}
       </span>
     );
   });
+
   return (
     <div id="countdown-area">
       <div className="container">
